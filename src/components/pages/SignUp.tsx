d42ce55aeb548a03112d95b2e27aec32d8e5c3b1
Shadee Merhi <shadmerhi@gmail.com>
2021-01-01 21:32:51 -0700

    added form wrapper

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,6 +1,6 @@
+import { useRef } from 'react';
 import { Typography } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField'
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
     borderRadius: '20px',
-    boxShadow: "1px 3px 5px 2px #F4F4F4",
+    boxShadow: "1px 4px 5px 2px #ededed",
     padding: '1rem 4rem 1rem 4rem',
     width: '40%',
   },
@@ -24,11 +24,11 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '1rem',
     fontFamily: 'halcom',
     fontSize: '20pt',
+    fontWeight: 500,
     color: '#FF5A5F'
   },
 
   field: {
-    width: '40%',
     height: '20px',
     margin: '0.5rem 0rem 0.5rem 0rem',
     fontFamily: 'halcom',
@@ -48,35 +48,47 @@ const useStyles = makeStyles((theme) => ({
       fontFamily: 'halcom',
       color: '#838383'
     }
+  },
+
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '50%'
   }
 }));
 
 export default function SignUp() {
 
   const classes = useStyles();
+  const emailRef = useRef();
+  // const passwordRef = useRef<HTMLDivElement>(null);
+  const passwordRef = useRef();
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <Typography className={classes.title}>Create an Account</Typography>
-        <input
-            className={classes.field}
-            required
-            type="text"
-            placeholder="Email"
-          />
-        <input
-            className={classes.field}
-            required
-            type="password"
-            placeholder="password"
-          />
-        <input
-            className={classes.field}
-            required
-            type="password"
-            placeholder="confirm password"
-          />
+        <form className={classes.form}>
+          <input
+              className={classes.field}
+              required
+              type="text"
+              placeholder="Email"
+              ref={emailRef.current}
+            />
+          <input
+              className={classes.field}
+              required
+              type="password"
+              placeholder="password"
+            />
+          <input
+              className={classes.field}
+              required
+              type="password"
+              placeholder="confirm password"
+            />
+        </form>
       </Paper>
     </div>
   );
