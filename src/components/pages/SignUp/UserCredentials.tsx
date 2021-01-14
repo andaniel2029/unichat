effb64aeb9a414a5a74f7d9bb12f3759f715bac0
Shadee Merhi <shadmerhi@gmail.com>
2021-01-14 00:44:29 -0700

    initial refactoring of moving form 1 into userCredentials comp

--- src/components/pages/SignUp/UserCredentials.tsx
+++ src/components/pages/SignUp/UserCredentials.tsx
@@ -1,121 +1,168 @@
-export {};
-// import { useState } from 'react';
-// import { Typography } from '@material-ui/core';
-// import Paper from '@material-ui/core/Paper';
-// import Button from '@material-ui/core/Button';
-// // import { UserCredentialsProps } from './SignUp';
-// import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
+import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
 
 
-// const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
 
-//   field: {
-//     height: '20px',
-//     width: '100%',
-//     margin: '0.5rem 0rem 0.5rem 0rem',
-//     fontFamily: 'halcom',
-//     fontSize: '12pt',
-//     border: '1px solid #E8E8E8',
-//     padding: '10px',
-//     borderRadius: '10px',
-//     transition: '0.2s ease-in-out',
-//     '&:focus': {
-//       outline: 'none',
-//       border: '1px solid #FF5A5F',
-//       borderRadius: '10px',
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderRadius: '20px',
+    boxShadow: "1px 4px 5px 2px #EDEDED",
+    padding: '1rem 0rem 1rem',
+    width: '320px',
+    height: '450px',
+    [theme.breakpoints.up('sm')]: {
+      width: '500px'
+    }
+  },
 
-//     },
-//     '&::placeholder': {
-//       fontSize: '12pt',
-//       fontFamily: 'halcom',
-//       color: '#838383'
-//     }
-//   },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '400px',
+  },
 
-//   form: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//     justifyContent: 'space-between',
-//   },
+  formInner: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
 
-//   btn: {
-//     margin: '1rem 0rem 1.5rem 0rem',
-//     fontFamily: 'halcom',
-//     color: 'white',
-//     background: '#FF5A5F',
-//     width: '100px',
-//     borderRadius: '20px',
-//     boxShadow: 'none',
-//     '&:hover': {
-//       background: '#FF5A5F',
-//     },
-//   },
+  programTitle: {
+    fontFamily: 'halcom',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '13pt'
+    }
+  },
 
-//   error: {
-//     color: '#FF5A5F'
-//   },
+  title: {
+    fontFamily: 'halcom',
+    fontSize: '20pt',
+    fontWeight: 500,
+    color: '#FF5A5F',
+  },
 
-//   inputContainer: {
-//     width: '130%',
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//     height: '200px',
-//     // border: '1px solid red',
-//   }
-// }));
+  field: {
+    height: '20px',
+    margin: '0.5rem 0rem 0.5rem 0rem',
+    fontFamily: 'halcom',
+    fontSize: '12pt',
+    border: '1px solid #E8E8E8',
+    padding: '10px',
+    borderRadius: '10px',
+    transition: '0.2s ease-in-out',
+    '&:focus': {
+      outline: 'none',
+      border: '1px solid #FF5A5F',
+      borderRadius: '10px',
 
-// export default function UserCredentials(props: UserCredentialsProps) {
+    },
+    '&::placeholder': {
+      fontSize: '12pt',
+      fontFamily: 'halcom',
+      color: '#838383'
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: '300px'
+    }
+  },
 
-//   const classes = useStyles();
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [passwordConfirm, setPasswordConfirm] = useState('');
-//   const [error, setError] = useState('');
+  btn: {
+    margin: '0.5rem 0rem 1rem 0rem',
+    fontFamily: 'halcom',
+    color: 'white',
+    background: '#FF5A5F',
+    width: '100px',
+    borderRadius: '20px',
+    boxShadow: 'none',
+    '&:hover': {
+      background: '#FF5A5F',
+    },
+  },
 
-//   const handleSubmit = function(e: any, email: string, password: string, passwordConfirm: string) {
-//     e.preventDefault();
-//     if(password !== passwordConfirm) {
-//       return setError('Passwords do not match');
-//     }
+  error: {
+    fontFamily: 'halcom',
+    textAlign: 'center',
+    color: '#FF5A5F'
+  },
 
-//     setError('');
-//     props.submit(e, email, password);
-//   }
+  spinner: {
+    marginTop: '4rem',
+    fontSize: '4rem',
+    color: '#FF5A5F'
+  }
+}));
 
-//   return (
-//     <form className={classes.form} onSubmit={event => handleSubmit(event, email, password, passwordConfirm)}>
-//       <div className={classes.inputContainer}>
-//         <input
-//             className={classes.field}
-//             required
-//             type="text"
-//             placeholder="Email"
-//             value={email}
-//             onChange={event => setEmail(event.target.value)}
-//           />
-//         <input
-//             className={classes.field}
-//             required
-//             type="password"
-//             placeholder="password"
-//             value={password}
-//             onChange={event => setPassword(event.target.value)}
-//           />
-//         <input
-//             className={classes.field}
-//             required
-//             type="password"
-//             placeholder="confirm password"
-//             value={passwordConfirm}
-//             onChange={event => setPasswordConfirm(event.target.value)}
-//           />
-//           {error && <Typography className={classes.error}>{error}</Typography>}
-//         </div>
-//       <Button variant="contained" type="submit" className={classes.btn}>Next</Button>
-//     </form>
-//   );
-// }
+interface Props {
+  setEmail: (email: string) => void,
+  setFirstName: (firstName: string) => void,
+  setLastName: (lastName: string) => void,
+  setPassword: (password: string) => void,
+  setPasswordConfirm: (passwordConfirm: string) => void,
+  submitCredentials: any,
+  error: string
+}
+
+export default function UserCredentials(props: Props) {
+
+  const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  // const handleSubmit = function(e: any, email: string, password: string, passwordConfirm: string) {
+  //   e.preventDefault();
+  //   if(password !== passwordConfirm) {
+  //     return setError('Passwords do not match');
+  //   }
+
+  //   setError('');
+  // }
+
+  return (
+    <form className={classes.form} onSubmit={props.submitCredentials(firstName, lastName, email, password, passwordConfirm)}>
+      <div className={classes.inputContainer}>
+        <input
+            className={classes.field}
+            required
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        <input
+            className={classes.field}
+            required
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        <input
+            className={classes.field}
+            required
+            type="password"
+            placeholder="confirm password"
+            value={passwordConfirm}
+            onChange={event => setPasswordConfirm(event.target.value)}
+          />
+          {props.error && <Typography className={classes.error}>{props.error}</Typography>}
+        </div>
+      <Button variant="contained" type="submit" className={classes.btn}>Next</Button>
+    </form>
+  );
+}
 
