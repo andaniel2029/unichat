a06ebb6f65aaa8403949cf049f31311deb98003e
Shadee Merhi <shadmerhi@gmail.com>
2021-01-14 13:55:54 -0700

    Logout button added - need /login route for redirection

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppButton from '../AppButton';
+import Button from '@material-ui/core/Button';
+import { Link, useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
+import { useAuth } from '../../hooks/useAuthContext';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,28 +18,55 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: 'halcom',
     fontSize: '28pt',
     color: '#FF5A5F'
+  },
+
+  button: {
+    fontFamily: 'halcom',
+    color: 'white',
+    background: '#FF5A5F',
+    width: '100px',
+    borderRadius: '20px',
+    boxShadow: 'none',
+    '&:hover': {
+    },
+  },
+
+  link: {
+    textDecoration: 'none',
+    '&:visited': {
+      textDecoration: 'none',
+    },
   }
 
 }));
 
-export interface BtnInput {
-  text: string,
-  url: string
-}
-
 export default function Nav() {
 
+  const { currentUser, logout } = useAuth();
+  const [error, setError] = useState('');
+  console.log('nav user', currentUser);
+  const history = useHistory();
 
-  const join:BtnInput = {
-    text: 'Join',
-    url: '/signup'
+  const handleLogout = function() {
+    setError('');
+    logout()
+    .then(() => {
+      history.push('/login');
+    })
+    .catch((error: any) => {
+      setError('Unable to logout')
+    })
   }
+
 
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Typography className={classes.logo}>UniChat</Typography>
-      <AppButton {...join}/>
+      {<Link to="/signup" className={classes.link}>
+        <Button variant="contained" className={classes.button}>Join</Button>
+      </Link>}
+      <Button variant="contained" className={classes.button} onClick={handleLogout}>Logout</Button>
     </div>
   )
 }
