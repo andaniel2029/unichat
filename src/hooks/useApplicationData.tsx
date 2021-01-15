d57e1e1b62dceb8577585f7e68bacd37e3b93cab
Shadee Merhi <shadmerhi@gmail.com>
2021-01-15 08:24:02 -0700

    Course interface added, courses added to app state

--- src/hooks/useApplicationData.tsx
+++ src/hooks/useApplicationData.tsx
@@ -2,16 +2,24 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export interface Program {
-  id: number | null,
-  name: string | null,
+  id: number | null;
+  name: string | null;
+}
+
+export interface Course {
+  id: number;
+  name: string;
+  color_code: string;
 }
 
 export default function useApplicationData() {
 
   const programs:Program[] = [];
+  const courses:Course[] = [];
 
   const [state, setState] = useState({
     programs,
+    courses,
     error: false
   })
 
