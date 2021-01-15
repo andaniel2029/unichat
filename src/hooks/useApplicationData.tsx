c2ef2d62eaf73a0597027994e521146e20ef3cb4
Shadee Merhi <shadmerhi@gmail.com>
2021-01-15 08:38:46 -0700

    courses received from API and set to app state

--- src/hooks/useApplicationData.tsx
+++ src/hooks/useApplicationData.tsx
@@ -28,9 +28,14 @@ export default function useApplicationData() {
       axios.get<Program[]>('/api/programs'),
       axios.get<Course[]>('/api/courses'),
     ]).then((all) => {
-      // SET THE APP STATE
+      console.log('programs', all[0].data)
+      console.log('courses', all[1].data)
+      setState({
+        ...state,
+        programs: all[0].data,
+        courses: all[1].data
+      })
     })
-    .then()
     .catch(error => {
       setState({
         ...state,
