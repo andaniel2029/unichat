7703464d21c5ae2b69c4b9420c1a3ebdeb61214a
Shadee Merhi <shadmerhi@gmail.com>
2021-01-18 16:40:09 -0700

    SocketContext created - app level socket

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -1,1 +1,38 @@
 import React, { useContext, useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { setSourceMapRange } from 'typescript';
+
+
+const SocketContext = React.createContext();
+
+const ENDPOINT = process.env.REACT_APP_SOCKET_URL;
+console.log(ENDPOINT);
+
+export function useSocket() {
+  return useContext(SocketContext);
+}
+
+export function SocketProvider({ id, children }) {
+
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    const newSocket = io(ENDPOINT);
+
+    setSocket(newSocket);
+
+    return () => {
+      if(!socket) return;
+      socket.off();
+    }
+  }, [id]);
+
+  console.log(socket);
+
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  )
+}
