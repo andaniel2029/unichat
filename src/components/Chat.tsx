3e0d43384d0fb1fe70198d8dbd692774fe54e817
Shadee Merhi <shadmerhi@gmail.com>
2021-01-19 16:30:37 -0700

    socket added to useEffect dependency array

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,20 +1,30 @@
 import React, { useEffect } from 'react';
 import { useSocket } from '../contexts/SocketProvider';
+import { useAuth } from '../hooks/useAuthContext';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
 
 export default function Chat() {
 
   const { socket } = useSocket();
+  const { currentUser } = useAuth();
   console.log('the socket from the chat', socket);
 
   useEffect(() => {
     if(!socket) return;
-    socket.emit('sendMessage', ('hello there'))
-  }, [])
+    socket.emit('sendMessage', ('hello there'));
+
+    return () => socket.off();
+  }, [socket]);
 
 
   return (
     <div>
-      Chat Page    
+      <Typography>Chat Page</Typography>
+      <Link to='/'>
+        <Button variant="outlined">Home</Button>
+      </Link>
     </div>
   )
 }
