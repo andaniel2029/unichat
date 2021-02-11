0c25d5b6f825607d126d5ce158e3aa8b93f8bf97
Shadee Merhi <shadmerhi@gmail.com>
2021-02-11 01:32:18 -0700

    tutor room section layout complete - no api call

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 // React
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 // Components
 import CourseItem from './CourseItem';
@@ -12,6 +12,7 @@ import { useAppData } from '../../../contexts/AppDataProvider';
 import { Theme, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
 
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -21,17 +22,47 @@ const useStyles = makeStyles((theme: Theme) => ({
     alignItems: 'center',
   },
 
-  chatRoomContainer: {
-    width: '80%'
+  text: {
+    fontFamily: 'halcom',
   },
 
-  courseTitle: {
+  tutorRoomsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  noTutorsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
+  noTutorsText: {
+    fontSize: '14pt'
+  },
+
+  noTutorsIcon: {
+    fontSize: '50pt',
+    color: '#ACABAB',
+    marginTop: '1rem',
+  },
+
+  chatRoomContainer: {
+    // width: '80%',
+    margin: '2rem 0rem 2rem 0rem',
+    background: '#FCFCFC'
+  },
+
+  sectionTitle: {
     fontFamily: 'halcom',
     fontSize: '26pt',
     marginBottom: '1.5rem',
     borderBottom: '2px solid #FF5A5F',
   }
 }));
+
+const tutors = [1, 2, 3, 4];
 
 export default function Home() {
 
@@ -41,6 +72,9 @@ export default function Home() {
   // Context variables
   const { currentUser } = useAuth();
   const { courses } = useAppData();
+
+  // State
+  const [availableTutors, setAvailableTutors] = useState([]);
 
   // Testing token authentication with Firebase - to be implemented
   useEffect(() => {
@@ -52,12 +86,24 @@ export default function Home() {
 
   return (
     <div className={classes.root}>
-      <Typography className={`${classes.courseTitle}`}>Chat Rooms</Typography>
-      <Grid container justify="center" className={classes.chatRoomContainer}>
-        {courses.map((course:any) => {
-          return <CourseItem key={course.id} course={course} home={true}/>
-        })}
+      <Grid container justify="center" className={classes.tutorRoomsContainer}>
+        <Typography className={`${classes.text} ${classes.sectionTitle}`}>Available Tutors</Typography>
+        {availableTutors.length === 0 && (
+        <div className={classes.noTutorsContainer}>
+          <Typography className={`${classes.text} ${classes.noTutorsText}`}>There are no available tutors right now!</Typography>
+          <SentimentDissatisfiedIcon className={classes.noTutorsIcon}/>
+        </div>
+        )}
       </Grid>
+      <Grid container justify="center" className={classes.chatRoomContainer}>
+        <Typography className={`${classes.text} ${classes.sectionTitle}`}>Chat Rooms</Typography>
+        <Grid container justify="center">
+          {courses.map((course:any) => {
+            return <CourseItem key={course.id} course={course} home={true}/>
+          })}
+        </Grid>
+      </Grid>
+
     </div>
   );
 }
