393c96bacb7971d63893725c52d95d40521a5953
David <davemgj@gmail.com>
2021-02-11 18:11:37 -0800

    initial commit by David Jardine - starting feature 'become tutor'

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -1,11 +1,11 @@
 // React
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 
 // Contexts
-import { useAuth } from '../hooks/useAuthContext';
+import { useAuth } from "../hooks/useAuthContext";
 
 // Socket IO
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 const SocketContext = React.createContext();
 
@@ -16,28 +16,25 @@ export function useSocket() {
 }
 
 export function SocketProvider({ children }) {
-
   const [socket, setSocket] = useState();
   const { currentUser } = useAuth();
-  
-  useEffect(() => {
-    
-    if(currentUser.user) {
-      const id = currentUser.user.uid;
-      const newSocket = io(ENDPOINT, 
-        { query: { id } });
-        setSocket(newSocket);
-      }
 
-      return () => {
-      if(!socket) return;
-      socket.off();
+  useEffect(() => {
+    if (currentUser.user) {
+      const id = currentUser.user.uid;
+      const newSocket = io(ENDPOINT, { query: { id } });
+      setSocket(newSocket);
     }
+
+    return () => {
+      if (!socket) return;
+      socket.off();
+    };
   }, [currentUser.user]);
 
   return (
     <SocketContext.Provider value={{ socket }}>
       {children}
     </SocketContext.Provider>
-  )
+  );
 }
