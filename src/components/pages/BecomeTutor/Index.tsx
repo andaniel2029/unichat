66b67ad59e5ec6829cccbe27d4062a21aca75614
Shadee Merhi <shadmerhi@gmail.com>
2021-02-13 18:20:11 -0700

    added tutorCourses state and loading state w spinner

--- src/components/pages/BecomeTutor/Index.tsx
+++ src/components/pages/BecomeTutor/Index.tsx
@@ -5,14 +5,15 @@ import React, { useState, useEffect } from 'react';
 
 // Contexts and Hooks
 import { useAuth } from '../../../hooks/useAuthContext';
-import { useAppData } from '../../../contexts/AppDataProvider';
+// import { useAppData } from '../../../contexts/AppDataProvider';
 
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Theme, Paper } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // Other libraries
-// import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const useStyles = makeStyles((theme: Theme) => ({
 
@@ -77,6 +78,13 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
   },
 
+
+  loadingSpinner: {
+    marginTop: '4rem',
+    fontSize: '4rem',
+    color: '#FF5A5F'
+  },
+
   '@keyframes fadeInSlide': {
     '0%': {
       opacity: 0,
@@ -96,15 +104,27 @@ export default function BecomeTutor() {
 
   // Context variables
   const { currentUser } = useAuth();
-  const { tutorCourses } = useAppData();
+  // const { tutorCourses } = useAppData();
 
   // State
+  const [loading, setLoading] = useState(true);
+  const [tutorCourses, setTutorCourses] = useState([]);
   const [selectedYear, setSelectedYear] = useState('First Year');
   const [error, setError] = useState(false);
 
-
+  // Retrieving all course data from the API
+  useEffect(() => {
+    axios.get('/api/courses/tutorcourses').then((res: AxiosResponse) => {
+      setTutorCourses(res.data);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000)
+    })
+    .catch((error:AxiosError) => setError(true));
+  }, []);
+  
   console.log(tutorCourses);
-
+  
   return (
     <div className={classes.root}>
       <div className={classes.headerContainer}>
@@ -117,14 +137,20 @@ export default function BecomeTutor() {
       </div>
       {error && <Typography>There was an error</Typography>}
       <Paper className={classes.courseSelectorContainer}>
-        <Typography className={`${classes.text} ${classes.courseSelectorHeaderText}`}>What courses would you like to assist with?</Typography>
-        <Typography className={classes.text}>Select a year to view available courses</Typography>
-        <div className={classes.toggleYearContainer}>
-          <Paper/>
-          <Paper/>
-          <Paper/>
-        </div>
+        {!loading ? (
+          <>
+            <Typography className={`${classes.text} ${classes.courseSelectorHeaderText}`}>What courses would you like to assist with?</Typography>
+            <Typography className={classes.text}>Select a year to view available courses</Typography>
+            <div className={classes.toggleYearContainer}>
+              <Paper/>
+              <Paper/>
+              <Paper/>
+            </div>
+          </>
+        ) : <CircularProgress className={classes.loadingSpinner} size={100}/>}   
       </Paper>
     </div>
   )
 }
+
+
