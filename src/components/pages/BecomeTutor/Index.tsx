988b80629069ddd793616fce52b8c0adc77c9134
Shadee Merhi <shadmerhi@gmail.com>
2021-02-13 18:08:59 -0700

    moved api call to app level to AppDataProvider

--- src/components/pages/BecomeTutor/Index.tsx
+++ src/components/pages/BecomeTutor/Index.tsx
@@ -5,15 +5,17 @@ import React, { useState, useEffect } from 'react';
 
 // Contexts and Hooks
 import { useAuth } from '../../../hooks/useAuthContext';
+import { useAppData } from '../../../contexts/AppDataProvider';
 
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Theme, Paper } from '@material-ui/core';
 
 // Other libraries
-import axios, { AxiosError, AxiosResponse } from 'axios';
+// import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const useStyles = makeStyles((theme: Theme) => ({
+
   root: {
     display: 'flex',
     flexDirection: 'column',
@@ -64,6 +66,17 @@ const useStyles = makeStyles((theme: Theme) => ({
     borderBottom: '2px solid #FF5A5F',
   },
 
+  toggleYearContainer: {
+    display: 'flex',
+    '& > *': {
+      height: theme.spacing(6),
+      width: theme.spacing(16),
+      margin: theme.spacing(2),
+      background: '#F7F7F7',
+      boxShadow: 'none'
+    }
+  },
+
   '@keyframes fadeInSlide': {
     '0%': {
       opacity: 0,
@@ -83,17 +96,14 @@ export default function BecomeTutor() {
 
   // Context variables
   const { currentUser } = useAuth();
+  const { tutorCourses } = useAppData();
 
   // State
+  const [selectedYear, setSelectedYear] = useState('First Year');
   const [error, setError] = useState(false);
 
-  // Retrieving all course data from the API
-  useEffect(() => {
-    axios.get('/api/courses/tutorcourses').then((res: AxiosResponse) => {
-      console.log(res.data);
-    })
-    .catch((error:AxiosError) => setError(true));
-  }, []);
+
+  console.log(tutorCourses);
 
   return (
     <div className={classes.root}>
@@ -109,6 +119,11 @@ export default function BecomeTutor() {
       <Paper className={classes.courseSelectorContainer}>
         <Typography className={`${classes.text} ${classes.courseSelectorHeaderText}`}>What courses would you like to assist with?</Typography>
         <Typography className={classes.text}>Select a year to view available courses</Typography>
+        <div className={classes.toggleYearContainer}>
+          <Paper/>
+          <Paper/>
+          <Paper/>
+        </div>
       </Paper>
     </div>
   )
