6f0ac9adce80f8243b93ec44604a3bc4f71c6090
Shadee Merhi <shadmerhi@gmail.com>
2021-02-15 01:19:22 -0700

    Paper added, root styling added

--- src/components/pages/BecomeTutor/SubjectItem.tsx
+++ src/components/pages/BecomeTutor/SubjectItem.tsx
@@ -3,23 +3,49 @@ import React from 'react';
 
 // Material UI
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) => ({
   root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: theme.spacing(4),
+    width: theme.spacing(8),
+    margin: theme.spacing(2),
+    background: props => props.selected ? '#FF5A5F' : '#F7F7F7',
+    color: props => props.selected ? 'white' : '#454545',
+    boxShadow: 'none',
+    transition: '0.2s ease-in-out',
+    '&:hover': {
+      cursor: 'pointer',
+      color: 'white',
+      background: '#FF5A5F'
+    }
+  },
 
+  text: {
+    fontFamily: 'halcom'
   }
 
 }));
 
+interface StyleProps {
+  selected: boolean;
+}
+
 interface Props {
   subject: string;
+  selected: boolean;
 }
 
 export default function SubjectItem(props: Props) {
+
+  const classes = useStyles(props);
+
   return (
-    <div>
-      <Typography>{props.subject}</Typography>
-    </div>
+    <Paper className={classes.root}>
+      <Typography className={classes.text}>{props.subject}</Typography>
+    </Paper>
   )
 }
