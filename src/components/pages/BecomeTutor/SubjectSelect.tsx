8a5e96367c0f80128da144b0ca32e8aa91658a6d
Shadee Merhi <shadmerhi@gmail.com>
2021-02-21 14:07:58 -0700

    props removed, using context vars

--- src/components/pages/BecomeTutor/SubjectSelect.tsx
+++ src/components/pages/BecomeTutor/SubjectSelect.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 // Components and Interfaces
 import { SubjectItem } from './SubjectItem';
+import { useTutorCourses } from '../../../hooks/useTutorCourses';
 
 // Material UI
 import { makeStyles, Theme } from '@material-ui/core';
@@ -21,26 +22,21 @@ interface StyleProps {
   subjects: string[];
 }
 
+export const SubjectSelect: React.FC = React.memo(() => {
 
-interface Props {
-  subjects: string[];
-  selectedSubject: string;
-  setSubject: (subject: string) => void;
-}
-
-export const SubjectSelect: React.FC<Props> = React.memo(({ subjects, selectedSubject, setSubject }) => {
-
+  // Context variables
+  const { state: { subjects, subject }, setSubject } = useTutorCourses();
   // Styles
   const classes = useStyles({ subjects });
 
   return (
     <Grid container justify="center" className={classes.root}>
-      {subjects.map(subject => {
+      {subjects.map(subjectName => {
         return (
         <SubjectItem
-          key={subject}
-          subject={subject}
-          selected={subject === selectedSubject}
+          key={subjectName}
+          subject={subjectName}
+          selected={subjectName === subject}
           setSubject={setSubject}
         />
       )
