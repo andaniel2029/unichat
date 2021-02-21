ed36e6a4dff32761ab4bb85da462a00a6aefdb14
Shadee Merhi <shadmerhi@gmail.com>
2021-02-21 13:58:54 -0700

    Props removed, context now used

--- src/components/pages/BecomeTutor/YearSelect.tsx
+++ src/components/pages/BecomeTutor/YearSelect.tsx
@@ -4,37 +4,34 @@ import React from 'react';
 // Components and Interfaces
 import { YearItem } from './YearItem';
 
+// Context and hooks
+import { useTutorCourses } from '../../../hooks/useTutorCourses';
+
 // Material UI
 import { makeStyles, Theme } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles<Theme>((theme: Theme) => ({
-
   root: {
-
   }
 }));
 
+export const YearSelect: React.FC = React.memo(() => {
 
-interface Props {
-  years: string[];
-  selectedYear: string;
-  setYear: (year: string) => void;
-}
-
-export const YearSelect: React.FC<Props> = React.memo(({ years, selectedYear, setYear }) => {
+  // Context variables
+  const { state: { allCourses, year }, setYear } = useTutorCourses();
 
   // Styles
   const classes = useStyles();
 
   return (
     <Grid container justify="center" className={classes.root}>
-      {years.map(year => {
+      {(Object.keys(allCourses)).map(yearName => {
         return (
         <YearItem
-          key={year}
-          year={year} 
-          selected={year === selectedYear}
+          key={yearName}
+          year={yearName} 
+          selected={yearName === year}
           setYear={setYear}
         />
       )
